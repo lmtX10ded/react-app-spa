5451642d1aca9e63a0bc96bc8b45ae14327eab6f
My Computer <mycomputer@Kishans-iMac.local>
2018-06-21 17:00:26 -0400

    i

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//Ref https://www.kirupa.com/react/creating_single_page_app_react_using_react_router.htm
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main";
+import "./index.css";
+
+ReactDOM.render(
+  <Main/>,
+  document.getElementById("root")
+);
